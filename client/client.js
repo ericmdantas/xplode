89f6ef524d19319e69be8c6a692867f872e146f7
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-16 18:55:03 -0300

    .

--- client/client.js
+++ client/client.js
@@ -1,11 +1,10 @@
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 1000; i++) {
     let wk = new Worker('./wk.js');
 
     wk.postMessage('yo!');
 
     wk.onmessage = function(ev) {
         console.log('omg');
-        console.log(ev);
     }
 }
 
--- client/wk.js
+++ client/wk.js
@@ -1,8 +1,3 @@
 self.onmessage = function(ev) {
     self.postMessage(":D")
-
-    fetch("http://jsonplaceholder.typicode.com/todos/1")
-        .then((res) => {
-            console.log(res);
-        });  
 }
